0e4b8bb4b79edbfff901e939d03cd743371361d1
Ittiwat tuntithavorn <ittiwat_tun@live.com>
2017-03-20 17:50:20 +0700

    [Add] TestJsObject

--- CloudScript.js
+++ CloudScript.js
@@ -3,3 +3,25 @@ handlers.HelloWorld = function(args) {
 	result.message = "hello world";
 	return result;
 }
+
+handlers.TestJsObject - function(args) {
+
+	var result = {};
+	var tracker = JSON.parse("{\"LoginStreak\":1,\"NextEligibleGrant\":1490090129539}");
+
+	log.info("tracker");
+	log.info(tracker);
+
+	log.info(".");
+	log.info(tracker.LoginStreak);
+	log.info(tracker.NextEligibleGrant);
+
+	log.info(".Value");
+	log.info(tracker["LoginStreak"]);
+	log.info(tracker["NextEligibleGrant"]);
+
+	log.info("end tracker");
+
+
+	return result;
+}
