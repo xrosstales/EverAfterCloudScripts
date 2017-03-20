f8253e910747ac2a76fe80d4aaebc1ea41ae56c7
Ittiwat tuntithavorn <ittiwat_tun@live.com>
2017-03-20 18:38:25 +0700

    [Add] TestJsObjectTwo

--- CloudScript.js
+++ CloudScript.js
@@ -24,3 +24,33 @@ handlers.TestJsObject = function(args) {
 
 	return result;
 }
+
+handlers.TestJsObjectTwo = function(args) {
+
+	var GetUserReadOnlyDataRequest = {
+		"PlayFabId": currentPlayerId,
+		"Keys": [CHECK_IN_TRACKER]
+	};
+	var GetUserReadOnlyDataResponse = server.GetUserReadOnlyData(GetUserReadOnlyDataRequest);
+
+	// need to ensure that our data field exists
+	var tracker = {}; // this would be the first login ever (across any title), so we have to make sure our record exists.
+	if (GetUserReadOnlyDataResponse.Data.hasOwnProperty(CHECK_IN_TRACKER)) {
+		tracker = JSON.parse(GetUserReadOnlyDataResponse.Data[CHECK_IN_TRACKER].Value);
+
+		log.info("tracker");
+		log.info(tracker);
+
+		log.info(".");
+		log.info(tracker.LoginStreak);
+		log.info(tracker.NextEligibleGrant);
+
+		log.info(".Value");
+		log.info(tracker["LoginStreak"]);
+		log.info(tracker["NextEligibleGrant"]);
+
+		log.info("end tracker");
+	}
+
+	return result;
+}
