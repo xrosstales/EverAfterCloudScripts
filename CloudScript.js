182ee08a359304a85eaa674fefd1305b66b74910
Ittiwat tuntithavorn <ittiwat_tun@live.com>
2017-03-21 18:12:11 +0700

    [Mod] test function

--- CloudScript.js
+++ CloudScript.js
@@ -53,47 +53,7 @@ handlers.TestJsObjectTwo = function(args) {
 
 		log.info("end tracker");
 
-	} else {
-
-		tracker = ResetTracker();
-
-		log.info("tracker");
-		log.info(tracker);
-
-		log.info("get field by .");
-		log.info(tracker.LoginStreak);
-		log.info(tracker.NextEligibleGrant);
-
-		log.info("get field by []");
-		log.info(tracker["LoginStreak"]);
-		log.info(tracker["NextEligibleGrant"]);
-
-		log.info("end tracker");
-		
-		UpdateTrackerData(tracker);
 	}
 
 	return result;
-
-	function ResetTracker() {
-		var reset = {};
-
-		reset[TRACKER_LOGIN_STREAK] = 1;
-
-		var dateObj = new Date(Date.now());
-		dateObj.setDate(dateObj.getDate() + 1); // add one day 
-
-		reset[TRACKER_NEXT_GRANT] = dateObj.getTime();
-		return JSON.stringify(reset);
-	}
-
-	function UpdateTrackerData(data) {
-		var UpdateUserReadOnlyDataRequest = {
-			"PlayFabId": currentPlayerId,
-			"Data": {}
-		};
-		UpdateUserReadOnlyDataRequest.Data[CHECK_IN_TRACKER] = JSON.stringify(data);
-
-		server.UpdateUserReadOnlyData(UpdateUserReadOnlyDataRequest);
-	}
 }
